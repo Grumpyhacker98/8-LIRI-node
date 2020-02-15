e208624609510afc300438d5cb6f3035303611ee
Max Lewis <max.a.lewis98@gmail.com>
2020-02-14 19:06:42 -0700

    movie and concerts are working, spotify key gives error

--- liri.js
+++ liri.js
@@ -16,12 +16,77 @@
 
 require("dotenv").config()
 
+const axios = require('axios');
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 
+var searchAction = String(process.argv[2])
+var searchName = String(process.argv[3])
+
+var bandKey = "https://rest.bandsintown.com/artists/" + searchName + "/events?app_id=codingbootcamp"
+var omdbKey = "http://www.omdbapi.com/?t=" + searchName + "&y=&plot=short&apikey=trilogy"
+
+if (searchAction === "concert-this") {
+    axios.get(bandKey)
+        .then(function (response, error) {
+            if (error) {
+                console.log(error)
+            }
+            console.log("===============================")
+            for(var i=0;i<response.data.length;i++){
+                v = i + 1
+                console.log("Event Number: "+v)
+                console.log("country: "+response.data[i].venue.country)
+                console.log("Venue Name: "+response.data[i].name)
+                console.log("Date: "+response.data[i].datetime)
+                console.log("===============================")
+            }
+        })
+}
+if (searchAction === "spotify-this") {
+    spotify.search({ type: searchName, query: 'All the Small Things' }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data);
+    });
+}
+if (searchAction === "movie-this") {
+    axios.get(omdbKey)
+        .then(function (response, error) {
+            if (error) {
+                console.log(error)
+            }
+            console.log("Title: " + response.data.Title)
+            console.log("Year: " + response.data.Year)
+            console.log("Ratings: " + response.data.Ratings)
+            console.log("Country: " + response.data.Country)
+            console.log("Language: " + response.data.Language)
+            console.log("Plot: " + response.data.Plot)
+            console.log("Actors: " + response.data.Actors)
+        })
+}
+if (searchAction === "do-what-it-says") {
+    console.log("test1")
+}
+
 // liri command line 1 inputs
-// concert-this
-// spotify-this-song
-// movie-this
-// do-what-it-says
+    // concert-this <artist/band name here>
+        // search bandsintownAPI give venue name location date
+    // spotify-this-song song
+        // search spotifyAPI and give artists name album previous link
+    // movie-this
+
+    // do-what-it-says
+        // run command from RandomSource.txt
+
+// nodeSpotify
+
+
+// axios
+
+
+
